650ef3d3830e79d830fe5361f3815e484f785208
BeardedToo <viktor.ilijasic@gmail.com>
2016-11-04 21:48:47 +0100

    Updated template and view

--- blog1/views.py
+++ blog1/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from blog1.models import Post # TODO: wildcard this, both app and model
 
 # Create your views here.
 
 def post_list(request):
+	#posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts = Post.objects.all()
 	return render(request, 'blog/post_list.html', {})
 
