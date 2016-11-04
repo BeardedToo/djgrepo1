3a452e68d08c94bcf13937a49f2779c416ead0ba
BeardedToo <viktor.ilijasic@gmail.com>
2016-11-04 03:59:11 +0100

    commit 2

---
+++ README.md
@@ -0,0 +1,1 @@
+# djgrepo1
---
+++ quicktests/helloworld.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  helloworld.py
+#  
+
+print('Hello Py!')
+
+if 3>2:
+	print("3 is greater than 2!")
+	
+def hi(name):
+	print("Hi, " + name + "!")
+	
+hi("Jack");
+
+i=1
+
+for i in range(1,10):
+	print("Value: " + str(i))
+	
