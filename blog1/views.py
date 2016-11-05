f6a1b45ad927d7ddf5254199f94070411e7453d9
BeardedToo <viktor.ilijasic@gmail.com>
2016-11-05 04:26:11 +0100

    Added post view and template, updated style.

--- blog1/views.py
+++ blog1/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import *
 
@@ -6,6 +6,11 @@ from .models import *
 
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	now = timezone.now()
 	#posts = Post.objects.all()
-	return render(request, 'blog/post_list.html', {'posts': posts})
+	return render(request, 'blog/post_list.html', {'posts': posts, 'now': now})
 
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	now = timezone.now()
+	return render(request, 'blog/post_detail.html', {'post': post, 'now': now})
