77f85ba51247627bbd633722891c0c5c6e436467
BeardedToo <viktor.ilijasic@gmail.com>
2016-11-15 04:19:51 +0100

    Added method to unpublish a post.

--- blog1/models.py
+++ blog1/models.py
@@ -7,11 +7,15 @@ class Post(models.Model):
 	text = models.TextField()
 	created_date = models.DateTimeField(default=timezone.now)
 	published_date = models.DateTimeField(blank=True, null=True)
-	
+
 	def publish(self):
 		self.published_date = timezone.now()
 		self.save()
-		
+
+	def unpublish(self):
+		self.published_date = None
+		self.save()
+
 	def __str__(self):
 		return self.title
-		
+
